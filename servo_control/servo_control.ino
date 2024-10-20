3400f97db620d3a2776ea5223d4fc761f00d5ee0
Berkay Kol <89808875+berkaykl@users.noreply.github.com>
2024-10-20 23:43:09 +0300

    Update servo_control.ino

--- servo_control/servo_control.ino
+++ servo_control/servo_control.ino
@@ -1,20 +1,32 @@
 #include <Servo.h>
 
 Servo servo1;
+Servo servo2;
 int servo1pin = 9;
-int gelenDeger;
-int deger;
-
+int servo2pin = 10;
 
 void setup() {
   Serial.begin(9600);
   servo1.attach(servo1pin);
+  servo2.attach(servo2pin);
 }
 
 void loop() {
-  if(Serial.available() > 0) {
-    gelenDeger = Serial.parseInt();
-    deger = map(gelenDeger, 0, 480, 0, 180);
-    servo1.write(deger);
+  if (Serial.available() > 0) {
+    String data = Serial.readStringUntil('\n');
+
+    int commaIndex = data.indexOf(',');
+
+    if (commaIndex > 0) {
+      String xVal = data.substring(0, commaIndex);
+      
+      String yVal = data.substring(commaIndex + 1);
+
+      int xPos = xVal.toInt();
+      int yPos = yVal.toInt();
+      
+      servo1.write(map(xPos, 0, 640, 0, 180));
+      servo2.write(map(yPos, 0, 480, 0, 180));
+    }
   }
 }
